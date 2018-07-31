53bf64c37adc4b7fed09c6d622952ea57614cf1f
Himalee <himalee.tailor@gmail.com>
2018-07-31 14:28:03 +0100

    adds test for ComputerPlayer

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -2,11 +2,27 @@ require "player"
 require "computer_player"
 
 describe ComputerPlayer do
-  it "returns number between 1 and 9" do
-    board = Board.new(3)
+
+  before :each do
+    @board = Board.new(3)
     message = Message.new
     display = Display.new(message)
-    computer_player = ComputerPlayer.new(board, display)
-    expect(computer_player.get_cell).to be_between(1, 9)
+    @computer_player = ComputerPlayer.new(@board, display)
+  end
+
+  it "returns number between 1 and 9" do
+    expect(@computer_player.get_cell).to be_between(1, 9)
+  end
+
+  it "returns number 5 given marked board" do
+    @board.mark_board(1, "X")
+    @board.mark_board(2, "X")
+    @board.mark_board(3, "X")
+    @board.mark_board(4, "X")
+    @board.mark_board(6, "X")
+    @board.mark_board(7, "X")
+    @board.mark_board(8, "X")
+    @board.mark_board(9, "X")
+    expect(@computer_player.get_cell).to eql(5)
   end
 end
