c4df22b96e3a8b86e6968793fc4992df38b8c34d
Himalee <himalee.tailor@gmail.com>
2018-08-01 14:08:39 +0100

    adds test for ComputerPlayer vs HumanPlayer

--- spec/integration/game_spec.rb
+++ spec/integration/game_spec.rb
@@ -17,33 +17,55 @@ describe Game do
     @validator = MoveValidator.new(@board)
   end
 
-  it "returns marked board given numbers from console" do
-    input = StringIO.new("1\n4\n2\n5\n3")
-    console = Console.new(@output, input)
-    display = Display.new(console, @message, @validator)
-    players = [HumanPlayer.new(@board, display, Peg::PLAYER_ONE_MARK), HumanPlayer.new(@board, display, Peg::PLAYER_TWO_MARK)]
-    game = Game.new(display, @board, players)
-    game.play
-    expect(@output.string).to include(" X │ X │ X \n───┼───┼───\n O │ O │ 6 \n───┼───┼───\n 7 │ 8 │ 9\n")
+  context "human vs human game" do
+    it "returns marked board given numbers from console" do
+      input = StringIO.new("1\n4\n2\n5\n3")
+      console = Console.new(@output, input)
+      display = Display.new(console, @message, @validator)
+      players = [HumanPlayer.new(@board, display, Peg::PLAYER_ONE_MARK), HumanPlayer.new(@board, display, Peg::PLAYER_TWO_MARK)]
+      game = Game.new(display, @board, players)
+      game.play
+      expect(@output.string).to include(" X │ X │ X \n───┼───┼───\n O │ O │ 6 \n───┼───┼───\n 7 │ 8 │ 9\n")
+    end
+
+    it "returns win with invalid choices" do
+      input = StringIO.new("1\n1\n4\nhello\n2\n5\n7\n6")
+      console = Console.new(@output, input)
+      display = Display.new(console, @message, @validator)
+      players = [HumanPlayer.new(@board, display, Peg::PLAYER_ONE_MARK), HumanPlayer.new(@board, display, Peg::PLAYER_TWO_MARK)]
+      game = Game.new(display, @board, players)
+      game.play
+      expect(@output.string).to include("Player two wins")
+    end
+
+    it "returns draw" do
+      input = StringIO.new("1\n2\n3\n4\n6\n9\n7\n5\n8")
+      console = Console.new(@output, input)
+      display = Display.new(console, @message, @validator)
+      players = [HumanPlayer.new(@board, display, Peg::PLAYER_ONE_MARK), HumanPlayer.new(@board, display, Peg::PLAYER_TWO_MARK)]
+      game = Game.new(display, @board, players)
+      game.play
+      expect(@output.string).to include("draw")
+    end
   end
 
-  it "returns win with invalid choices" do
-    input = StringIO.new("1\n1\n4\nhello\n2\n5\n7\n6")
-    console = Console.new(@output, input)
-    display = Display.new(console, @message, @validator)
-    players = [HumanPlayer.new(@board, display, Peg::PLAYER_ONE_MARK), HumanPlayer.new(@board, display, Peg::PLAYER_TWO_MARK)]
-    game = Game.new(display, @board, players)
-    game.play
-    expect(@output.string).to include("Player two wins")
-  end
-
-  it "returns draw" do
-    input = StringIO.new("1\n2\n3\n4\n6\n9\n7\n5\n8")
-    console = Console.new(@output, input)
-    display = Display.new(console, @message, @validator)
-    players = [HumanPlayer.new(@board, display, Peg::PLAYER_ONE_MARK), HumanPlayer.new(@board, display, Peg::PLAYER_TWO_MARK)]
-    game = Game.new(display, @board, players)
-    game.play
-    expect(@output.string).to include("draw")
+  context "computer vs human game" do
+    it "returns player one wins" do
+      @board.mark_board(1, "X")
+      @board.mark_board(2, "O")
+      @board.mark_board(3, "X")
+      @board.mark_board(4, "O")
+      @board.mark_board(5, "X")
+      @board.mark_board(6, "O")
+      @board.mark_board(8, "X")
+      @board.mark_board(7, "O")
+      input = StringIO.new("2")
+      console = Console.new(@output, input)
+      display = Display.new(console, @message, @validator)
+      players = [ComputerPlayer.new(@board, display, Peg::PLAYER_ONE_MARK), HumanPlayer.new(@board, display, Peg::PLAYER_TWO_MARK)]
+      game = Game.new(display, @board, players)
+      game.play
+      expect(@output.string).to include("Player one wins")
+    end
   end
 end
