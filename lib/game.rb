343a593b95d1ed5c839e5426213382d9484350a1
Himalee <himalee.tailor@gmail.com>
2018-08-07 15:05:17 +0100

    removes dead code from Game

--- lib/game.rb
+++ lib/game.rb
@@ -22,26 +22,13 @@ class Game
   private
 
   def turns
-    until end_of_game?(@board.grid)
+    until @board.end_of_game?(@board.grid)
       @display.choose_cell
       number = current_player.get_cell
-      mark_board(number, current_player.mark)
+      @board.mark_board(number, current_player.mark)
       @display.present_board_with_squares(@board)
       next_player
     end
-  end
-
-  def end_of_game?(grid)
-    @board.end_of_game?(grid)
-  end
-
-  def mark_board(number, mark)
-    @board.mark_board(number, mark)
-  end
-
-  def new_board(grid, cell_number, mark)
-    grid[index_position(cell_number)] = mark
-    grid
   end
 
   def present_result
