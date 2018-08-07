b5875b2e55728e55dce3ed36b23558195e6b3974
Himalee <himalee.tailor@gmail.com>
2018-08-07 15:07:39 +0100

    refactors min_mac_spec tests

--- spec/min_max_spec.rb
+++ spec/min_max_spec.rb
@@ -8,56 +8,47 @@ require "mark"
 
 describe MinMax do
 
+  before :each do
+    board = Board.new(3)
+    @min_max = MinMax.new(board)
+  end
+
   context "X - maximizer" do
     it "one move left" do
-      board = Board.new(3)
       grid = ["X", "O", "X", "O", "O", "X", "X", 8, "O"]
-      min_max = MinMax.new(board)
-      expect(min_max.get_best_move(grid, "X")).to eql(8)
+      expect(@min_max.get_best_move(grid, "X")).to eql(8)
     end
 
     it "two moves left" do
-      board = Board.new(3)
       grid = ["X", "O", "O", "O", "X", "O", 7, "O", 9]
-      min_max = MinMax.new(board)
-      expect(min_max.get_best_move(grid, "X")).to eql(9)
+      expect(@min_max.get_best_move(grid, "X")).to eql(9)
     end
 
     it "three moves left" do
-      board = Board.new(3)
       grid = ["X", "X", 3, "O", "O", 6, 7, "O", "O"]
-      min_max = MinMax.new(board)
-      expect(min_max.get_best_move(grid, "X")).to eql(3)
+      expect(@min_max.get_best_move(grid, "X")).to eql(3)
     end
 
     it "four moves left" do
-      board = Board.new(3)
       grid = ["X", "O", 3, "O", "X", 6, "O", 8 , 9]
-      min_max = MinMax.new(board)
-      expect(min_max.get_best_move(grid, "X")).to eql(9)
+      expect(@min_max.get_best_move(grid, "X")).to eql(9)
     end
 
     it "five moves left" do
-      board = Board.new(3)
       grid = ["O", "X", 3, 4, 5, 6, 7, "X", "O"]
-      min_max = MinMax.new(board)
-      expect(min_max.get_best_move(grid, "X")).to eql(5)
+      expect(@min_max.get_best_move(grid, "X")).to eql(5)
     end
 
     it "trap" do
-      board = Board.new(3)
       grid = ["O", 2, 3, 4, "X", "O", 7, 8, "X"]
-      min_max = MinMax.new(board)
-      expect(min_max.get_best_move(grid, "X")).to eql(3).or(eq(7))
+      expect(@min_max.get_best_move(grid, "X")).to eql(3).or(eq(7))
     end
   end
 
   context "O - maximizer" do
     it "stopping opponent from winning" do
-      board = Board.new(3)
       grid = ["X", 2, "X", 4, "O", "O", 7, 8, 9]
-      min_max = MinMax.new(board)
-      expect(min_max.get_best_move(grid, "O")).to eql(2)
+      expect(@min_max.get_best_move(grid, "O")).to eql(2)
     end
   end
 end
